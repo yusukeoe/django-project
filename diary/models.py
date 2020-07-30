c4829d9cbb5aabce2fe7a954421255e4db2dbfa5
MBP030 <mbp030@zealsnoMacBook-Pro-110.local>
2020-07-30 19:22:13 +0900

    コミッットしちゃうわよん！

---
+++ diary/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone
+
+class Day(models.Model):
+    title = models.CharField('タイトル', max_length=200)
+    text = models.TextField('本文')
+    date = models.DateTimeField('日付', default=timezone.now)
